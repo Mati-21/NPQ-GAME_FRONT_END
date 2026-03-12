5a1a786360f1c1660c718021a57bbc620ad76f03
Mati Melkamu <matimelkamu15@gmail.com>
2026-03-12 18:44:27 +0300

    fixing some bug ingame session

--- src/pages/Game_Pages/GameSessionPage.jsx
+++ src/pages/Game_Pages/GameSessionPage.jsx
@@ -3,9 +3,9 @@ import ChatComponent from "./ChatComponent";
 function GameSessionPage() {
   const guessedNumbers = [2545, 1254, 2556, 2548, 2255, 6532];
   return (
-    <div className="flex px-10 py-4 shadow-all m-4 flex-1 gap-4">
+    <div className="flex-1 flex px-10 py-4 shadow-all m-4 gap-4">
       {/* Sidebar and Main Section  */}
-      <div className="flex-3  flex">
+      <div className="flex-3 flex">
         {/* Side Section */}
         <div className="flex-1">side</div>
         {/* Main Section */}
@@ -93,3 +93,43 @@ function GameSessionPage() {
 }
 
 export default GameSessionPage;
+
+//  <div class="flex flex-col h-screen">
+//       <div class="p-4 bg-blue-500 text-white">Header</div>
+
+//       <div class="flex-1 overflow-y-auto p-4">
+//         <p>Content 1</p>
+//         <p>Content 2</p>
+//         <p>Content 3</p>
+//         <p>Content 4</p>
+//         <p>Content 5</p>
+//         <p>Content 6</p>
+//         <p>Content 7</p>
+//         <p>Content 1</p>
+//         <p>Content 2</p>
+//         <p>Content 3</p>
+//         <p>Content 4</p>
+//         <p>Content 5</p>
+//         <p>Content 6</p>
+//         <p>Content 7</p>
+//         <p>Content 1</p>
+//         <p>Content 2</p>
+//         <p>Content 3</p>
+//         <p>Content 4</p>
+//         <p>Content 5</p>
+//         <p>Content 6</p>
+//         <p>Content 7</p>
+//         <p>Content 5</p>
+//         <p>Content 6</p>
+//         <p>Content 7</p>
+//         <p>Content 1</p>
+//         <p>Content 2</p>
+//         <p>Content 3</p>
+//         <p>Content 4</p>
+//         <p>Content 5</p>
+//         <p>Content 6</p>
+//         <p>Content 7</p>
+//       </div>
+
+//       <div class="p-4 bg-gray-200">Footer</div>
+//     </div>
